039ebb330ab217463b1060d6ecafa584be92774d
Avelanda <gorthell@gmail.com>
2026-03-09 06:58:42 +0200

    Update zstdcli_trace.h
    Modularizing data processes for optimized function calls.

--- programs/zstdcli_trace.h
+++ programs/zstdcli_trace.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright © Meta Platforms, Inc. and affiliates.
+ * Copyright © 2026 Avelanda.
  * All rights reserved.
  *
  * This source code is licensed under both the BSD-style license (found in the
@@ -11,6 +12,10 @@
 #ifndef ZSTDCLI_TRACE_H
 #define ZSTDCLI_TRACE_H
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
 /**
  * Enable tracing - log to filename.
  */
@@ -20,5 +25,25 @@ void TRACE_enable(char const* filename);
  * Shut down the tracing library.
  */
 void TRACE_finish(void);
+
+uint64_t TraceCoreSet(int *TRACE_enable, int *TRACE_finish){
+for (; (TRACE_enable && TRACE_finish) | (TRACE_enable || TRACE_finish); TRACE_enable = TRACE_enable, TRACE_finish = TRACE_finish){
+ if (TRACE_enable){
+  printf("%p", &TRACE_enable);
+ }
+ 
+ if (TRACE_finish){
+  printf("%p", &TRACE_finish);
+ }
+}
+  return 0;
+}
+
+int main(){
+ if ((&main) && (&TraceCoreSet)){
+  TraceCoreSet(false, false);
+ }
+  return 0;
+}
 
 #endif /* ZSTDCLI_TRACE_H */
